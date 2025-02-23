661d3b729d8c6a7060a4afe860526e12f346a3a1
Aria Bagheri <ariab9342@gmail.com>
2025-02-23 17:59:59 +0000

    added listener logic

--- src/postgres.rs
+++ src/postgres.rs
@@ -1,16 +1,54 @@
 use crate::CacheReserve;
 use persistent_postgres::{PgChangeAction, PgChangeNotification};
 use std::hash::Hash;
+use serde_json::Error;
+use tokio::sync::broadcast::Receiver;
+use tokio::task::JoinHandle;
 
 impl<PK, T> CacheReserve<PK, T>
 where
     PK: From<T> + Hash + Eq + Copy,
+    T: Clone
 {
-    pub fn listener(&self, notification: PgChangeNotification) {
-        match notification.action {
-            PgChangeAction::Update => {}
-            PgChangeAction::Delete => {}
-            PgChangeAction::Insert => {}
-        }
+    pub fn listener(&self, notification: PgChangeNotification, shutdown: Receiver<()>) -> JoinHandle<()> {
+        tokio::spawn(async move {
+            // Short circuit. If the record was just inserted, it clearly is not cached yet!
+            if let PgChangeAction::Insert = notification.action {
+                return
+            }
+            let record = match serde_json::from_value(notification.payload) {
+                Ok(v) => {
+                    v
+                },
+                Err(e) => {
+                    println!(
+                        "{} {}",
+                        "CACHE_RESERVE - PG_LISTENER - \
+                                Failed to deserialize change notification".red(),
+                        format!("| error = {}", e.to_string())
+                            .red()
+                            .dimmed()
+                    );
+                    return
+                }
+            };
+            match notification.action {
+                PgChangeAction::Update => {
+                    tokio::select! {
+                        _ = shutdown.recv();
+                        _ = self.update(PK::from(record.clone()), record).await;
+                    }
+                }
+                PgChangeAction::Delete => {
+                    tokio::select! {
+                        _ = shutdown.recv();
+                        _ = self.ignore(PK::from(record)).await;
+                    }
+                }
+                PgChangeAction::Insert => {
+                    unreachable!();
+                }
+            };
+        })
     }
 }
