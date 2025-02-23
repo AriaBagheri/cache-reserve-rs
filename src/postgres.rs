bb5d06b4459f5c46361f5d3471f86d9adadb2e4e
Aria Bagheri <ariab9342@gmail.com>
2025-02-23 17:42:37 +0000

    rustfmt

--- src/postgres.rs
+++ src/postgres.rs
@@ -1,17 +1,15 @@
-use std::hash::Hash;
-use persistent_postgres::{PgChangeAction, PgChangeNotification};
 use crate::CacheReserve;
+use persistent_postgres::{PgChangeAction, PgChangeNotification};
+use std::hash::Hash;
 
 impl<PK, T> CacheReserve<PK, T>
-    where PK: From<T> + Hash + Eq + Copy,
+where
+    PK: From<T> + Hash + Eq + Copy,
 {
     pub fn listener(&self, notification: PgChangeNotification) {
         match notification.action {
-            PgChangeAction::Update => {
-            }
-            PgChangeAction::Delete => {
-
-            }
+            PgChangeAction::Update => {}
+            PgChangeAction::Delete => {}
             PgChangeAction::Insert => {}
         }
     }
